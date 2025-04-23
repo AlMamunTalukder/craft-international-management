05302998289ddedf3714651ced73bb4917efec7e
AlMamunTalukder <almamuntalukder0@gmail.com>
2025-04-23 09:51:50 +0600

    add port and solve error

--- src/app/(dashboardLayout)/dashboard/super_admin/student/create/page.tsx
+++ src/app/(dashboardLayout)/dashboard/super_admin/student/create/page.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
-import StudentForm from '../_components/StudentForm';
+// import React from 'react';
+// import StudentForm from '../_components/StudentForm';
 
-const page = () => {
-  return <StudentForm />
+
+// import StudentForm from "../_components/StudentForm";
+
+// const page = () => {
+  //   return <StudentForm />
+  // };
+  
+  // export default page;
+
+
+
+import StudentForm from "../_components/StudentForm";
+
+const page = ({ params }: { params: { id: string } }) => {
+  return <StudentForm id={params.id} />
 };
-
 export default page;
+
+
+// import React from 'react';
+// import StudentForm from '../_components/StudentForm';
+
+// const page = ({ params }: { params: { id?: string } }) => {
+//   return <StudentForm id={params.id} />
+// };
+
+// export default page;
